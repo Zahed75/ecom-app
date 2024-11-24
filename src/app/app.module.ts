03918272c01c09ae0731ba8efa844fadc4c29ba3
“Zahed75” <zahedhasan.cs@gmail.com>
2024-11-24 20:25:35 +0600

    Products showed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {ProductModule} from './product/product.module';
 import {HttpClientModule} from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 
 @NgModule({
   declarations: [
@@ -14,6 +17,8 @@ import {HttpClientModule} from '@angular/common/http';
     AppRoutingModule,
     ProductModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ProductService} from '../product.service';
+import {Product} from "../../models/product";
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
 
+// calling api service
+export class ProductListComponent implements OnInit {
+  products: Product[] = []
+
+  constructor(private productService: ProductService) {
+
+  }
+
+  ngOnInit() {
+    this.productService.getProducts().subscribe(data => {
+      this.products = data;
+
+
+    })
+  }
 }
